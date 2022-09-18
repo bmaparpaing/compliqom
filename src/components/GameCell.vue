c86d76b9a341ba49f0593d27312999357786086e
bmaparpaing <ben.male@free.fr>
2022-09-18 15:04:36 +0200

    🎉 Initial commit

---
+++ src/components/GameCell.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  val: { letter: string; index: number };
+}>();
+</script>
+
+<template>
+  <div class="cell">
+    <div class="cell-content">{{ val.letter }}</div>
+  </div>
+</template>
+
+<style scoped>
+.cell {
+  border: 2px solid white;
+  width: 4rem;
+  height: 4rem;
+  font-size: 3rem;
+  font-family: Helvetica, sans-serif;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/GameGrid.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+import GameCell from "./GameCell.vue";
+
+const props = defineProps<{
+  gamegrid: Array<Array<{ letter: string; index: number }>>;
+}>();
+
+const styleObject = reactive({
+  gridTemplateColumns:
+      "repeat(" + props.gamegrid[0].length + ", minmax(0, 1fr))",
+  width: props.gamegrid[0].length * 4 + "rem",
+});
+</script>
+
+<template>
+  <div class="game-grid" :style="styleObject">
+    <template v-for="line in gamegrid">
+      <GameCell v-for="item in line" :val="item" :key="item.index"/>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.game-grid {
+  width: 16rem;
+  display: grid;
+}
+</style>
