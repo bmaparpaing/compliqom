b9f912a334d5eebf106e89bf22cd098263f4fcc0
bmaparpaing <ben.male@free.fr>
2022-10-01 18:11:05 +0200

    🚚 #17 Déplacement dictionnaire dans les assets publics
    Déplacement du dictionnaire dans le dossier public pour qu'il soit traité comme ressource statique et qu'il soit disponible dans le livrable.

--- src/dictionary-service.ts
+++ src/dictionary-service.ts
@@ -2,9 +2,7 @@ export class DictionaryService {
   static allowedWords: string[] = []; // la liste des mots autorisés pour la saisie par le joueur
 
   static async initDictionaryFromFile(): Promise<void> {
-    const allowedWordsFile: Response = await fetch(
-      "/src/assets/allowed_words.txt"
-    );
+    const allowedWordsFile: Response = await fetch("/assets/allowed_words.txt");
     const allowedWordsFileContent = await allowedWordsFile.text();
     this.allowedWords = allowedWordsFileContent.split("\n");
   }
